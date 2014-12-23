103f521b5d86a9bd3d13cce0a6fec8c4de73d033
chenjing123 <894027241@qq.com>
2014-12-23 21:14:05 +0800

    Create seach.c

---
+++ seach.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define FORMAT"%d\n%s\n%f\n%f\n%f\n"
+struct student 
+{int num;
+char name[20];
+float score[3];
+}stu={12345,"LiLi",67.5,89,78.6};
+
+void main()
+{void print(struct student *);
+ print(&stu);
+}
+
+void print(struct student *p)
+{printf(FORMAT,p->num,p->name,p->score[0],p->score[1],p->score[2]);
+ printf("\n");
+}
